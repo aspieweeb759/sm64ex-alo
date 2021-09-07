455a69d3f08e30c1930fea241d9cc84137caf522
Aspie Weeb <80162484+aspieweeb759@users.noreply.github.com>
2021-09-06 18:31:44 -0700

    Update ScrollTargets.inc.c

--- src/game/ScrollTargets.inc.c
+++ src/game/ScrollTargets.inc.c
@@ -5,7 +5,7 @@
 //Q. Why does this exist instead of just directly referencing VBs?
 //A. Because gcc is dumb and will seg fault if you reference a VB by abstracting it through a bparam
 //instead of directly refencing it, causing this horrible shit.
-extern Vtx VB_vcutm_1_0xe004860[];
+/*extern Vtx VB_vcutm_1_0xe004860[];
 Vtx *ScrollTargets[]={
  &VB_vcutm_1_0xe004860[255],
-};
+};*/
